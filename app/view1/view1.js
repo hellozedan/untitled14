06d60132a0451627edadede6bdbe56e08d882d3c
zedany7@hotmail.com <Alk0544282120>
2016-10-08 14:31:28 +0300

    Initial commit

---
+++ app/view1/view1.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', ['$scope',function($scope) {
+  var mainString=" ناموا في أحضان والديهم ولم يستيقظوا ومن بقي على قيد الحياة كاد قهره يقتله ليلعن العالم أجمع بلا إستثناء في ذلك اليوم كانت الجثث مترامية على مدى البصر في كل ثانية تلقى جثة برقم مجهولة في الارض معروفة في السماء لفناء عائلات بأكملها كان كرنفالا وحشيا أقصى ما صنعه أنه تسبب بقلق أممي لم يكن يرقى لحجم المأساة التي وقعت فتتالت اللعنات على صانعي الحقوق والمبادىء المزعومة بعد موقفهم الاستنكاري ليبقى مبدأنا هو الباقي لزوال شيء";
+  $scope.text=mainString.split(' ');
+  $scope.selected={};
+  $scope.select=function (index) {
+    if($scope.selected[index]){
+      delete $scope.selected[index];
+     }
+    else
+    {
+      $scope.selected[index]=true;
+    }
+  };
+
+  $scope.isSelected=function (index) {
+    if($scope.selected[index]){
+      return 'red'
+    }
+    else
+    {
+      'none'
+    }
+  };
+
+}]);
