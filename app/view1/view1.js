0382844742bbbf7d8d54d748aeb48615167391f5
zedany7@hotmail.com <Alk0544282120>
2016-10-09 18:53:34 +0300

    make it better

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -32,7 +32,7 @@ angular.module('myApp.view1', ['ngRoute'])
 			var dataToDb = {userData: angular.fromJson(userData),selected:$scope.selected,text:$scope.text,idea:$scope.idea};
 
 
-			$http.post('https://echochrome.herokuapp.com/api/model',
+			$http.post('https://researchxyz.herokuapp.com/api/model',
 				{data:dataToDb}
 				, {
 					headers: {
